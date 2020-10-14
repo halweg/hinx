eb0cf17a1672b6ac36e42b974d4e14cbe870dc23
halweg <halweg@126.com>
2020-10-14 23:24:40 +0800

    fix clientv0.7

--- zinxDemo/zinxv0.7/Server.go
+++ zinxDemo/zinxv0.7/Server.go
@@ -27,7 +27,7 @@ func (hr *HelloRouter) Handle(request ziface.IRequest) {
 
 func main() {
 
-	s := znet.NewZinxServer("[zinx0.6]")
+	s := znet.NewZinxServer("[zinx0.7]")
 
 	s.AddRouter(0, &PingRouter{})
 	s.AddRouter(1, &HelloRouter{})
